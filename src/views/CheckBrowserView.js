ced7c87feeb85313f7cbeefd44840ab5da92b8ed
Jay Chiang <jaychiang0809@berkeley.edu>
2023-01-27 05:37:12 -0800

    tiny little fix

--- src/views/CheckBrowserView.js
+++ src/views/CheckBrowserView.js
@@ -6,22 +6,29 @@ const CheckBrowserView = () => {
 
     useEffect(() => {
         // Opera 8.0+
-        // const isOpera = (!!window.opr) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+        const isOpera = (!!window.opr) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
 
         // Firefox 1.0+
-        // const isFirefox = typeof InstallTrigger !== 'undefined';
+        const isFirefox = typeof InstallTrigger !== 'undefined';
 
         // Safari 3.0+ "[object HTMLElementConstructor]" 
-        // const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
+        const isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && window['safari'].pushNotification));
 
         // Edge 20+
-        // const isEdge = !!window.StyleMedia;
+        const isEdge = !!window.StyleMedia;
 
         // Chrome 1 - 79
-        const isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+        const isChrome = !!window.chrome
 
         // Edge (based on chromium) detection
-        // const isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+        const isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+        console.log(`isOpera: ${isOpera}`)
+        console.log(`isFirefox: ${isFirefox}`)
+        console.log(`isSafari: ${isSafari}`)
+        console.log(`isEdge: ${isEdge}`)
+        console.log(`isChrome: ${isChrome}`)
+        console.log(`isEdgeChromium: ${isEdgeChromium}`)
 
         if (!isChrome) {
             setSupported(false)
